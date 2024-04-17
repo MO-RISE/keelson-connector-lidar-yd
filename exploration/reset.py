54a4d8584798fccedb67474d0289b74e6c4d0b6e
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-04-17 18:43:29 +0200

    sdk

--- exploration/reset.py
+++ exploration/reset.py
@@ -2,7 +2,7 @@ import PyLidar3
 
 port = "/dev/ttyACM1" 
 
-Obj = PyLidar3.YdLidarG4(port) #PyLidar3.your_version_of_lidar(port,chunk_size   
+Obj = PyLidar3.YdLidarG4(port)  
 
 if(Obj.Connect()):
     Obj.Reset()
