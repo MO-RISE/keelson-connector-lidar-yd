39e8305e5f02f08ae68c7f8cf8f88cfe665c4023
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-04-17 15:53:47 +0200

    Update reset.py

--- exploration/reset.py
+++ exploration/reset.py
@@ -4,6 +4,7 @@ port = "/dev/ttyACM1"
 
 Obj = PyLidar3.YdLidarX4(port) #PyLidar3.your_version_of_lidar(port,chunk_size   
 
-Obj.Reset()
+if(Obj.Connect()):
+    Obj.Reset()
 
-print("Resseted device")
+    print("Resseted device")
