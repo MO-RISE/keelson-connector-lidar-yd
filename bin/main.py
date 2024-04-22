59ed2b46cf340bb767914cd6f19d2f860562e5b9
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-04-22 12:36:47 +0200

    Update main.py

--- bin/main.py
+++ bin/main.py
@@ -87,11 +87,14 @@ if __name__ == "__main__":
     ports = ydlidar.lidarPortList()
     port = args.device_port
 
-    for key, value in ports.items():
-        port = value
-        logging.info(f"Conneting to port: {port}")
+    # for key, value in ports.items():
+    #     port = value
+    
+    logging.info(f"Conneting on port: {port}")
 
     laser = ydlidar.CYdLidar()
+    logging.info(f"Conneted: {port}")
+    
     laser.setlidaropt(ydlidar.LidarPropSerialPort, port)
     laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 230400)
     laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TRIANGLE)
@@ -107,81 +110,89 @@ if __name__ == "__main__":
 
     ret = laser.initialize()
 
-    if ret:
-        ret = laser.turnOn()
-        scan = ydlidar.LaserScan()
-        while ret and ydlidar.os_isOk():
+    try:
+        if ret:
+            ret = laser.turnOn()
+            scan = ydlidar.LaserScan()
 
-            ret = laser.doProcessSimple(scan)
+            while ret and ydlidar.os_isOk():
 
-            if ret:
-                ingress_timestamp = time.time_ns()
+                ret = laser.doProcessSimple(scan)
 
-                logging.debug(
-                    f"Scan received[{scan.stamp}]:{scan.points.size()} ranges is [{scan.config.scan_time*100}]Hz"
-                )
+                if ret:
+                    ingress_timestamp = time.time_ns()
 
-                # TODO: Laser scan Parsing and Publishing 
-                # readings = []
-                # for point in scan.points:
-                #     readings.append([point.angle, point.range])
+                    logging.debug(
+                        f"Scan received[{scan.stamp}]:{scan.points.size()} ranges is [{scan.config.scan_time*100}]Hz"
+                    )
 
-                # logging.debug(f"Readings: {len(readings)}")
-                # data = relative_positions.tobytes() ERROR 
-                # payload = LaserScan()
-                # payload.timestamp.FromNanoseconds(ingress_timestamp)
-                # payload.start_angle = 0 # Bearing of first point, in radians
-                # payload.start_angle = 6.26573 # Bearing of last point, in radians
-                # payload.ranges.extend(ranges_arr_float) 
-                # payload.pose.position.x = 0
-                # payload.pose.position.y = 0
-                # payload.pose.position.z = 0
-                # serialized_payload = payload.SerializeToString()
-                # envelope = keelson.enclose(serialized_payload)
-                # pub_scan.put(envelope)
-                    
-               
-                # Point Cloud Parsing and Publishing 
-                relative_positions = []
-                for point in scan.points:
-                    if point.range > 0: # Removing non returns (?)
-                        x = point.range * math.cos(point.angle)
-                        y = point.range * math.sin(point.angle)
-                        relative_positions.append([x, y, 0])
+                    # TODO: Laser scan Parsing and Publishing 
+                    # readings = []
+                    # for point in scan.points:
+                    #     readings.append([point.angle, point.range])
+
+                    # logging.debug(f"Readings: {len(readings)}")
+                    # data = relative_positions.tobytes() ERROR 
+                    # payload = LaserScan()
+                    # payload.timestamp.FromNanoseconds(ingress_timestamp)
+                    # payload.start_angle = 0 # Bearing of first point, in radians
+                    # payload.start_angle = 6.26573 # Bearing of last point, in radians
+                    # payload.ranges.extend(ranges_arr_float) 
+                    # payload.pose.position.x = 0
+                    # payload.pose.position.y = 0
+                    # payload.pose.position.z = 0
+                    # serialized_payload = payload.SerializeToString()
+                    # envelope = keelson.enclose(serialized_payload)
+                    # pub_scan.put(envelope)
+                        
                 
-                logging.debug(f"Points: {len(relative_positions)}")
-            
-                np_relative_pos = np.array(relative_positions)
-                data = np_relative_pos.tobytes()
-                point_stride = len(data) / len(np_relative_pos)
-            
-                payload = PointCloud()
-                payload.point_stride = int(point_stride)
-                payload.data = data
-                payload.timestamp.FromNanoseconds(ingress_timestamp)
-                payload.pose.position.x = 0
-                payload.pose.position.y = 0
-                payload.pose.position.z = 0
-                payload.pose.orientation.x = 0.0  
-                payload.pose.orientation.y = 0.0
-                payload.pose.orientation.z = 0.0
-                payload.pose.orientation.w = 1.0
-                # Fields are in float64 (8 bytes each)
-                payload.fields.add(name="x", offset=0, type=8)
-                payload.fields.add(name="y", offset=8, type=8)
-                payload.fields.add(name="z", offset=16, type=8)
-                if args.frame_id is not None:
-                    payload.frame_id = args.frame_id
-                serialized_payload = payload.SerializeToString()
-                envelope = keelson.enclose(serialized_payload)
-                pub_point_cloud.put(envelope)
-                logging.debug("...published point cloud to zenoh!")
+                    # Point Cloud Parsing and Publishing 
+                    relative_positions = []
+                    for point in scan.points:
+                        if point.range > 0: # Removing non returns (?)
+                            x = point.range * math.cos(point.angle)
+                            y = point.range * math.sin(point.angle)
+                            relative_positions.append([x, y, 0])
+                    
+                    logging.debug(f"Points: {len(relative_positions)}")
+                
+                    np_relative_pos = np.array(relative_positions)
+                    data = np_relative_pos.tobytes()
+                    point_stride = len(data) / len(np_relative_pos)
+                
+                    payload = PointCloud()
+                    payload.point_stride = int(point_stride)
+                    payload.data = data
+                    payload.timestamp.FromNanoseconds(ingress_timestamp)
+                    payload.pose.position.x = 0
+                    payload.pose.position.y = 0
+                    payload.pose.position.z = 0
+                    payload.pose.orientation.x = 0.0  
+                    payload.pose.orientation.y = 0.0
+                    payload.pose.orientation.z = 0.0
+                    payload.pose.orientation.w = 1.0
+                    # Fields are in float64 (8 bytes each)
+                    payload.fields.add(name="x", offset=0, type=8)
+                    payload.fields.add(name="y", offset=8, type=8)
+                    payload.fields.add(name="z", offset=16, type=8)
+                    if args.frame_id is not None:
+                        payload.frame_id = args.frame_id
+                    serialized_payload = payload.SerializeToString()
+                    envelope = keelson.enclose(serialized_payload)
+                    pub_point_cloud.put(envelope)
+                    logging.debug("...published point cloud to zenoh!")
 
-            else:
-                logging.debug("Failed to get Lidar Data")
+                else:
+                    logging.debug("Failed to get Lidar Data")
 
-            time.sleep(0.05)
+                time.sleep(0.05)
+            laser.turnOff()
+
+        laser.disconnecting()
+    
+    except Exception as e:
+        logging.error(f"Error: {e}")
         laser.turnOff()
-    laser.disconnecting()
+        laser.disconnecting()
 
     logging.info(f"END OF SCRIPT")
