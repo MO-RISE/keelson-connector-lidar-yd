afdb82f8c0000c968fc57d67e367ef47fbfc4ae2
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-04-23 14:14:50 +0200

    Update main.py

--- bin/main.py
+++ bin/main.py
@@ -19,7 +19,8 @@ from terminal_inputs import terminal_inputs
 from keelson.payloads.TimestampedFloat_pb2 import TimestampedFloat
 from keelson.payloads.TimestampedString_pb2 import TimestampedString
 from keelson.payloads.PointCloud_pb2 import PointCloud
-from keelson.payloads.Experimental_PointCloudSimplified_pb2 import PointCloudSimplified, RelPointsPosition
+from keelson.payloads.Experimental_PointCloudSimplified_pb2 import PointCloudSimplified 
+from keelson.payloads.RelPointsPosition_pb2 import RelPointsPosition
 from keelson.payloads.LaserScan_pb2 import LaserScan    
 
 # Global variable for Zenoh session
@@ -168,15 +169,16 @@ if __name__ == "__main__":
                     # Point Cloud Parsing and Publishing 
                     relative_positions = []
                     simple_points = []
+                    
                     for point in scan.points:
                         if point.range > 0: # Removing non returns (?)
                             x = point.range * math.cos(point.angle)
                             y = point.range * math.sin(point.angle)
                             relative_positions.append([x, y, 0])
 
-                            protoMsg = RelPointsPosition()
-                            protoMsg.coordinates = [float(x), float(y), float(0)]
-                            simple_points.append(protoMsg)
+                            relpos = RelPointsPosition()
+                            relpos.coordinates = [float(x), float(y), float(0)]
+                            simple_points.append(relpos)
                     
                     logging.debug(f"Points: {len(relative_positions)}")
                     
